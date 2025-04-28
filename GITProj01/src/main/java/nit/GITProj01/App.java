35d8824d7e2319ea8b2b6a5167d98a3c750eb011
nataraz <natarazjavaarena@gmail.com>
2025-04-28 20:43:59 +0530

    task1 committed

--- GITProj01/src/main/java/nit/GITProj01/App.java
+++ GITProj01/src/main/java/nit/GITProj01/App.java
@@ -6,5 +6,7 @@ package nit.GITProj01;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("task1");
+        
     }
 }
