a3a1080120253cec671fd8fe2aff62e677acc6e5
nataraz <natarazjavaarena@gmail.com>
2025-04-30 19:33:12 +0530

    Dev1- completed UPI Payment feature

---
+++ GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -0,0 +1,11 @@
+package com.nt.upi;
+
+public class UPIPaymentOperations {
+	
+	
+	public  String  doPayment(long phNo,double amount) {
+		return  amount+" amount is paid to phone number ::"+phNo;
+		
+	}
+
+}
