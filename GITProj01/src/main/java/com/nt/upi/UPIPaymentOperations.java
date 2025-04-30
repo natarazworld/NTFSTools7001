c2652928caad104879ed609acdb4efc76c15e053
nataraz <natarazjavaarena@gmail.com>
2025-04-30 20:11:25 +0530

    updating the tag1

--- GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
+++ GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -5,7 +5,9 @@ public class UPIPaymentOperations {
 	//Performs the UPI Payment
 	public  String  doPayment(long phNo,double amount) {
 		
+		// perform UPI Payment
 		return  amount+" amount is paid to @phone number ::"+phNo;
+		
 		
 	}
 
