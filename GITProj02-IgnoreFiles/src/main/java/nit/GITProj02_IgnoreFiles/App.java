ca61e30dcd68379e991ede1b6251a59f8d790745
nataraz <natarazjavaarena@gmail.com>
2025-04-30 20:35:01 +0530

    proj2 sharing

---
+++ GITProj02-IgnoreFiles/src/main/java/nit/GITProj02_IgnoreFiles/App.java
@@ -0,0 +1,10 @@
+package nit.GITProj02_IgnoreFiles;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
