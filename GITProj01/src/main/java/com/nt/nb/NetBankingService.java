377642cf0d8bff23d564ed1590fa0727a893796d
nataraz <natarazjavaarena@gmail.com>
2025-04-30 19:55:57 +0530

    TL  Reviewed  NB code

--- GITProj01/src/main/java/com/nt/nb/NetBankingService.java
+++ GITProj01/src/main/java/com/nt/nb/NetBankingService.java
@@ -3,8 +3,9 @@ package com.nt.nb;
 public class NetBankingService {
 	
 	
+	// By deafault performs RTGS  payment
 	public  String  transferMoney(long srcAccount, long destAccount, String ifscCode, double amount) {
-		return  amount+"  is transfering from "+srcAccount+ " to "+destAccount;
+		return  amount+"  is transfering from "+srcAccount+ " to   "+destAccount;
 	}
 
 }
