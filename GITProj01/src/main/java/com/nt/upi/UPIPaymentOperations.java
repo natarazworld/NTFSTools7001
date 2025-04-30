17b36db49f08a302e7bd33cb30876a3e6ffda764
nataraz <natarazjavaarena@gmail.com>
2025-04-30 19:36:11 +0530

    TL reviewed the B1 Code

--- GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
+++ GITProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -2,9 +2,10 @@ package com.nt.upi;
 
 public class UPIPaymentOperations {
 	
-	
+	//Performs the UPI Payment
 	public  String  doPayment(long phNo,double amount) {
-		return  amount+" amount is paid to phone number ::"+phNo;
+		
+		return  amount+" amount is paid to @phone number ::"+phNo;
 		
 	}
 
